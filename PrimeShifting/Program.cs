0da7ae1b4c5c3e891335ce15a2da96888d28c57e
Daniel Lindsey <daniellindsey85254@gmail.com>
2022-02-23 13:36:40 -0800

    refactor into nested for loops
    Co-authored-by: Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
    Co-authored-by: Eric Crudup <Cruduper@users.noreply.github.com>

--- PrimeShifting/Program.cs
+++ PrimeShifting/Program.cs
@@ -5,16 +5,10 @@ namespace Primes
 {
   public class Program
   {
-    /*
-      Get number from the user -> n
-      for loop that starts at 2 and ends at n to add the numbers in the list
-      For each through the list to remove the multiples of 2 except the number 2
-      */
     public static void Main()
     {
       Console.WriteLine("Enter a number");
       int input = int.Parse(Console.ReadLine());
-      // List<int> primeList;
       List<int> numList = new List<int>();
       for (int x = 2; x <= input; x++)
       {
@@ -22,44 +16,20 @@ namespace Primes
       }
       for (int n = 2; n <= input; n++)
       {
-        //primeList.Add(2);
-        if ((n % 2) == 0)
-        //|| (n % 3) == 0 || (n % 4) == 0)
+        for (int x = 2; x < n; x++)
         {
-          numList.Remove(n);
-
+          if ((n % x) == 0)
+          {
+            numList.Remove(n);
+          }
         }
-        if ((n % 3) == 0)
-        {
-          numList.Remove(n);
-
-        }
-        if ((n % 5) == 0)
-        {
-          numList.Remove(n);
-
-        }
-        if ((n % 7) == 0)
-        {
-          numList.Remove(n);
-
-        }
-
       }
-      //list 1-100
-      //newlist--->add 2
-      numList.Add(2);
-      numList.Add(3);
-      numList.Add(5);
-      numList.Add(7);
       numList.Sort();
       foreach (int number in numList)
       {
         Console.WriteLine(number);
       }
-
     }
-
   }
 }
 
